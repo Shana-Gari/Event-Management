e958c7dbf10a7ab5cfa1cfc43c81fcc15f86e9d3
ShanaGari <shanagari7@gmail.com>
2025-03-02 16:09:40 +0530

    added corsheaders, rest_framework, and event(app) to INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'events',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
