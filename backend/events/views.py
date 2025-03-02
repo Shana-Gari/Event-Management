8f7840e0e21919498e0a8771134b706965475f0e
ShanaGari <shanagari7@gmail.com>
2025-03-02 21:06:01 +0530

    updated views

--- backend/events/views.py
+++ backend/events/views.py
@@ -14,6 +14,7 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]  # Allow public user registration
+    
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
