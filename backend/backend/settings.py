d23bc4fbb3a6b98484d8803ca556bd75efd5d766
ShanaGari <shanagari7@gmail.com>
2025-03-02 17:09:44 +0530

    added 'rest_framework_simplejwt' to INSTALLED_APPS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',
     'events',
     'django.contrib.admin',
     'django.contrib.auth',
