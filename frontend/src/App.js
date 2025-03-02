84237cafb16419d5809c6c96297771138bab87ec
ShanaGari <shanagari7@gmail.com>
2025-03-02 16:24:49 +0530

    connecting React to Django

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://127.0.0.1:8000/api/events/")
+            .then(response => setEvents(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h1>Upcoming Events</h1>
+            {events.map(event => (
+                <div key={event.id}>
+                    <h2>{event.title}</h2>
+                    <p>{event.description}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export default App;
