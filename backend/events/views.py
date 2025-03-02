93d9f2b02cc0f7159243e393bb42a6029ac97336
ShanaGari <shanagari7@gmail.com>
2025-03-02 16:27:45 +0530

    created api views

--- backend/events/views.py
+++ backend/events/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Event, Registration
+from .serializers import EventSerializer, RegistrationSerializer
 
-# Create your views here.
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow read access to everyone, but write access to authenticated users
+
+class RegistrationViewSet(viewsets.ModelViewSet):
+    queryset = Registration.objects.all()
+    serializer_class = RegistrationSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
