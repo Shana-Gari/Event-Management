65839733869b61ab35401747b462f000372747d8
ShanaGari <shanagari7@gmail.com>
2025-03-02 19:16:28 +0530

    updated permissions.py

--- backend/events/permissions.py
+++ backend/events/permissions.py
@@ -1,13 +1,7 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
-    """
-    Custom permission to allow only the creator to edit or delete an event.
-    """
-
     def has_object_permission(self, request, view, obj):
-        # Read permissions (GET, HEAD, OPTIONS) are allowed for everyone
-        if request.method in SAFE_METHODS:
-            return True
-        # Write permissions (PUT, DELETE) are allowed only if the user is the creator
-        return obj.created_by == request.user
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True  # Allow read access to everyone
+        return obj.created_by == request.user  # Only creator can edit/delete
