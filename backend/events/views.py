cf506cf5f3a2d2d469c0930467d7f55de54488a5
ShanaGari <shanagari7@gmail.com>
2025-03-02 19:14:07 +0530

    updated views.py

--- backend/events/views.py
+++ backend/events/views.py
@@ -2,11 +2,16 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Event, Registration
 from .serializers import EventSerializer, RegistrationSerializer
+from .permissions import IsOwnerOrReadOnly  # Import the custom permission
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]  # Allow read access to everyone, but write access to authenticated users
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # Apply custom permission
+
+    def perform_create(self, serializer):
+        # Ensure the logged-in user is set as the event creator
+        serializer.save(created_by=self.request.user)
 
 class RegistrationViewSet(viewsets.ModelViewSet):
     queryset = Registration.objects.all()
