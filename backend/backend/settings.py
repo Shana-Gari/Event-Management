7c5c5adbd4b23b16b06f1383843a77906ae4380e
ShanaGari <shanagari7@gmail.com>
2025-03-02 16:11:17 +0530

    Added corsheaders.middleware.CorsMiddleware to MIDDLEWARE, enables CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'corsheaders.middleware.CorsMiddleware',
+] + MIDDLEWARE #Enable CORS
 
 ROOT_URLCONF = 'backend.urls'
 
